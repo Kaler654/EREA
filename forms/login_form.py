8c9c477fb976382ea388a841e22741ac3b3d0934
Smart developer <kaler654@mail.ru>
2022-09-22 02:23:13 +0300

    add auth and book_view

---
+++ forms/login_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    """Форма регистрации"""
+    name = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_login import login_user, login_required, logout_user, current_user
 from forms.register_form import RegisterForm
+from forms.login_form import LoginForm
 from data.users import User
 from data import db_session
 import datetime
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'N#pC@UzmS5kw%@$F'
@@ -11,6 +13,16 @@ app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
     days=365
 )
 
+#  Инициализация LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Получение текущего пользователся"""
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -43,10 +55,31 @@ def reqister():
     return render_template('register.html', title='Регистрация', form=form)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Форма авторизации"""
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.name == form.name.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=True)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("homepage.html")
+
+
+@app.route('/book')
+def book():
+    return render_template("book_view.html")
 
 
 if __name__ == '__main__':
